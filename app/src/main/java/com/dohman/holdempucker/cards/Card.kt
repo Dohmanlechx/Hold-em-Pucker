010748c92dcc580678e5dee1507181858cddadef
unknown <dohman_92@hotmail.com>
2019-02-16 11:57:48 +0100

    Images for the cards added. Every card now has its imgsrc-string.

--- app/src/main/java/com/dohman/holdempucker/cards/Card.kt
+++ app/src/main/java/com/dohman/holdempucker/cards/Card.kt
@@ -3,4 +3,5 @@ package com.dohman.holdempucker.cards
 class Card {
     var suit: Suits? = null
     var rank: Int? = null
+    var src: String? = null
 }
--- app/src/main/java/com/dohman/holdempucker/cards/CardDeck.kt
+++ app/src/main/java/com/dohman/holdempucker/cards/CardDeck.kt
@@ -14,12 +14,30 @@ class CardDeck {
                     val card = Card()
                     card.suit = it
                     card.rank = i
+                    card.src = getImageStr(it, i)
                     cardDeck.add(card)
                 }
             }
         }
 
         return cardDeck
+    }
+
+    private fun getImageStr(suit: Suits, rank: Int): String {
+        val suitFirstChar: Char = suit.toString().first().toLowerCase()
+
+        val rankAsStr: String = if (rank <= 10) {
+            rank.toString()
+        } else {
+            when (rank) {
+                11 -> "j"
+                12 -> "q"
+                13 -> "k"
+                else -> "a"
+            }
+        }
+
+        return "$suitFirstChar$rankAsStr"
     }
 
     companion object {
