e6869b8efb4d5960a82b798a01cb12e99c23057f
unknown <dohman_92@hotmail.com>
2019-02-19 22:17:23 +0100

    The ViewModel working perfect. Ready for merge.

--- app/src/main/java/com/dohman/holdempucker/GameViewModel.kt
+++ app/src/main/java/com/dohman/holdempucker/GameViewModel.kt
@@ -1,7 +1,80 @@
 package com.dohman.holdempucker
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.dohman.holdempucker.cards.Card
+import com.dohman.holdempucker.cards.CardDeck
 
-class GameViewModel : ViewModel() {
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+    private val cardDeck = CardDeck().cardDeck
+    private var pickedCard: Card = cardDeck.first()
+    lateinit var currentCard: Card
 
+    val pickedCardNotifier = MutableLiveData<Int>()
+    val cardsCountNotifier = MutableLiveData<Int>()
+
+    val nfyBtmGoalie = MutableLiveData<Boolean>()
+
+    init {
+        showPickedCard()
+    }
+
+    // Private functions
+    private fun setResIdForNewPickedCard() {
+        pickedCardNotifier.value = resIdOfCard(pickedCard)
+    }
+
+    private fun setGoalie(isBottom: Boolean) {
+        when (isBottom) {
+            true -> nfyBtmGoalie.value = true
+        }
+
+    }
+
+    private fun removeCardFromDeck() {
+        cardDeck.remove(pickedCard)
+        pickedCard = cardDeck.first()
+        cardsCountNotifier.value = cardDeck.size
+    }
+
+    private fun takeNewCardFromDeck() {
+        //pickedCard = null
+        showPickedCard()
+    }
+
+    private fun isGoalieThere(goalieCard: Card, team: Array<Card?>): Boolean {
+        team.let { if (!it.all { element -> element == null}) return true else it[5] = goalieCard }
+        setGoalie(isBottom = true)
+        removeCardFromDeck()
+
+        return false // But goalie is added now
+    }
+
+    // Public functions
+
+    fun showPickedCard() {
+        if (!isGoalieThere(pickedCard, GameActivity.teamBottom)) { // If returned false, goalie is added
+            takeNewCardFromDeck()
+            return
+        }
+
+        pickedCard.let {
+            setResIdForNewPickedCard()
+            currentCard = it
+            removeCardFromDeck()
+        }
+
+        if (cardDeck.isEmpty()) {
+            //halfTime() // FIXME
+        }
+    }
+
+    fun resIdOfCard(card: Card): Int {
+        return card.let {
+            getApplication<Application>().resources.getIdentifier(
+                it.src, "drawable", getApplication<Application>().packageName
+            )
+        }
+    }
 }
