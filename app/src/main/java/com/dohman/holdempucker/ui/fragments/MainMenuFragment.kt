7e6aedda8353beed34c66d74ad529ffde4ff3628
unknown <dohman_92@hotmail.com>
2019-05-04 15:55:22 +0200

    Removed a folder and added gradient for title text, for fun.

--- app/src/main/java/com/dohman/holdempucker/ui/fragments/MainMenuFragment.kt
+++ app/src/main/java/com/dohman/holdempucker/ui/fragments/MainMenuFragment.kt
@@ -1,5 +1,8 @@
 package com.dohman.holdempucker.ui.fragments
 
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -11,7 +14,7 @@ import androidx.navigation.Navigation
 import com.dohman.holdempucker.R
 import kotlinx.android.synthetic.main.main_menu_fragment.*
 
-class MainMenuFragment : Fragment() {
+class MainMenuFragment : Fragment(), View.OnClickListener {
     private lateinit var vm: MainMenuViewModel
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -26,11 +29,36 @@ class MainMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tv_vs_friend.setOnClickListener { goToGameFragment(it) }
+        setOnClickListeners()
+        setGradientOnTexts()
     }
 
-    private fun goToGameFragment(view: View) {
-        Navigation.findNavController(view).navigate(R.id.action_mainMenuFragment_to_gameFragment)
+    private fun setGradientOnTexts() {
+        val titleWidth = tv_title.paint.measureText(tv_title.text.toString())
+        tv_title.paint.shader = LinearGradient(
+            0f, 0f, titleWidth, tv_title.textSize,
+            intArrayOf(
+                Color.parseColor("#F97C3C"),
+                Color.parseColor("#FDB54E"),
+                Color.parseColor("#64B678"),
+                Color.parseColor("#478AEA"),
+                Color.parseColor("#8446CC")
+            ),
+            null, Shader.TileMode.CLAMP
+        )
     }
 
+    private fun goToGameFragment(view: View?) {
+        view?.let { Navigation.findNavController(it).navigate(R.id.action_mainMenuFragment_to_gameFragment) }
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            tv_vs_friend -> goToGameFragment(v)
+        }
+    }
+
+    private fun setOnClickListeners() {
+        tv_vs_friend.setOnClickListener(this)
+    }
 }
