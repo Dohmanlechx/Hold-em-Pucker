eed63f3c2d9d5390978a2bc9a9f7b83ed63b03fc
unknown <dohman_92@hotmail.com>
2019-05-04 17:23:49 +0200

    Added OnTouch for swipe, and nice shadow effect for the text. Branch done.

--- app/src/main/java/com/dohman/holdempucker/ui/fragments/MainMenuFragment.kt
+++ app/src/main/java/com/dohman/holdempucker/ui/fragments/MainMenuFragment.kt
@@ -1,12 +1,10 @@
 package com.dohman.holdempucker.ui.fragments
 
-import android.graphics.Color
-import android.graphics.LinearGradient
-import android.graphics.Shader
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
@@ -14,51 +12,56 @@ import androidx.navigation.Navigation
 import com.dohman.holdempucker.R
 import kotlinx.android.synthetic.main.main_menu_fragment.*
 
-class MainMenuFragment : Fragment(), View.OnClickListener {
+class MainMenuFragment : Fragment(), View.OnTouchListener {
     private lateinit var vm: MainMenuViewModel
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        vm = ViewModelProviders.of(this).get(MainMenuViewModel::class.java)
-    }
+    private var isAlreadyCalled = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        vm = ViewModelProviders.of(this).get(MainMenuViewModel::class.java)
+
         return inflater.inflate(R.layout.main_menu_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setOnClickListeners()
+        setOnTouchListeners()
         setGradientOnTexts()
     }
 
+    override fun onResume() {
+        super.onResume()
+        isAlreadyCalled = false
+    }
+
     private fun setGradientOnTexts() {
-        val titleWidth = tv_title.paint.measureText(tv_title.text.toString())
-        tv_title.paint.shader = LinearGradient(
-            0f, 0f, titleWidth, tv_title.textSize,
-            intArrayOf(
-                Color.parseColor("#F97C3C"),
-                Color.parseColor("#FDB54E"),
-                Color.parseColor("#64B678"),
-                Color.parseColor("#478AEA"),
-                Color.parseColor("#8446CC")
-            ),
-            null, Shader.TileMode.CLAMP
-        )
+        tv_title.paint.shader = vm.getLinearGradient(tv_title)
     }
 
-    private fun goToGameFragment(view: View?) {
-        view?.let { Navigation.findNavController(it).navigate(R.id.action_mainMenuFragment_to_gameFragment) }
-    }
-
-    override fun onClick(v: View?) {
-        when (v) {
-            tv_vs_friend -> goToGameFragment(v)
+    private fun goToGameFragment() {
+        if (!isAlreadyCalled) {
+            isAlreadyCalled = true
+            view?.let { Navigation.findNavController(it).navigate(R.id.action_mainMenuFragment_to_gameFragment) }
         }
     }
 
-    private fun setOnClickListeners() {
-        tv_vs_friend.setOnClickListener(this)
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (event == null || v == null) return false
+
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                val fingerX = event.rawX - (v.width / 2)
+                v.x = fingerX
+                if (fingerX >= tv_vs_friend.width.toFloat()) goToGameFragment()
+            }
+            MotionEvent.ACTION_UP -> v.performClick()
+        }
+
+        return true
+    }
+
+    private fun setOnTouchListeners() {
+        puck_vs_friend.setOnTouchListener(this)
     }
 }
