d7f9c0ce5b87d4557328c208f40f54d4dc9187e4
David Öhman <dohman_92@hotmail.com>
2019-05-17 13:48:27 +0200

    SplashFragment instead has an own layout.

--- app/src/main/java/com/dohman/holdempucker/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/dohman/holdempucker/ui/splash/SplashFragment.kt
@@ -2,10 +2,13 @@ package com.dohman.holdempucker.ui.splash
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.navigation.Navigation
 
 import com.dohman.holdempucker.R
 
@@ -14,8 +17,13 @@ class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         vm = ViewModelProviders.of(this).get(SplashViewModel::class.java)
-
         return inflater.inflate(R.layout.splash_fragment, container, false)
     }
 
+    override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(v, savedInstanceState)
+        Handler().postDelayed({
+            view?.let { Navigation.findNavController(it).navigate(SplashFragmentDirections.actionSplashFragmentToMainMenuFragment()) }
+        }, 2000)
+    }
 }
